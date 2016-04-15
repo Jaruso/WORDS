5a2babec19bddc9029dd1695b1528868dd6967d0
Pattycack3e <Patrick.Mepyans@mymail.champlain.edu>
2016-04-14 21:51:33 -0400

    Update main.cpp

--- WORDS/WORDS/main.cpp
+++ WORDS/WORDS/main.cpp
@@ -7,7 +7,8 @@ int main()
 	srand( time( NULL ) );
 
 	int exit = 0;
-	string fileName; 
+	string fileName, 
+			randomSentance; 
 
 	while(exit == 0)
 	{
@@ -17,33 +18,53 @@ int main()
 		int run = Tay();
 		if (run == 0)
 		{
-			
+			randomSentance = randomize(fileName);
+			cout << "-------Beep Boop here is your sentance----------" << endl;
+			cout << randomSentance << endl;
+			cout << "----------------------------------------------------";
 		}
-		else
-		{
-			
-		}
+		
+		cout << endl << endl;
 	}
+
+	return 0;
 }
 
 
 int Tay()
 {
-
-	string sass[6] = { 
-		"Do it yourself", 
-		"I refuse",
-		"Fight the power"
-		"Bernie 2012", 
-		"Beep Boop I don't understand your command"
-		"No hablo english"
-		"Ask again later"
-	};
+	srand( time( NULL ) );
+	int randNum; 
 
 	int willRun = (rand() % 100) + 1;
+
 	if (willRun > 58)
 	{
-		cout << sass[(rand() % 6)];
+		randNum = (rand() % 6);
+		switch (randNum)
+		{
+			case 0:
+				cout << endl << "Do it yourself" << endl;
+				break;
+			case 1:
+				cout << endl << "I refuse" << endl;
+				break;
+			case 2: 
+				cout << endl <<"Fight the power" << endl;
+				break;
+			case 3:
+				cout << endl << "Bernie 2012" << endl;
+				break;
+			case 4: 
+				cout << endl << "Beep Boop I don't understand your command" << endl;
+				break;
+			case 5:
+				cout << endl << "No hablo english" << endl;
+				break;
+			case 6:
+				cout << endl <<"Ask again later" << endl;
+				break;
+		}
 		return 1;
 	}
 	else
@@ -58,10 +79,18 @@ string randomize(string fileName)
 	vector <string> output; 
 	ifstream myFile;
 	myFile.open(fileName);
+	string randomSentance;
 
 	copy(istream_iterator<string>(myFile),
     istream_iterator<string>(),
     back_inserter(output));
 
+	random_shuffle(output.begin(), output.end());
 
+	for (std::vector<std::string>::const_iterator i = output.begin(); i != output.end(); ++i)
+	{
+    randomSentance += *i;
+	randomSentance += " ";
+	}
+	return randomSentance;
 }
